307a1cb4409a69cb3fa486196ced3e9a32f845e6
Ibrahim Mohammed <ibrahimmohdtk@gmail.com>
2025-12-11 14:05:16 +0530

    deleted prisma config

---
+++ prisma/migrations/20251211083436_removed_streak_reduction_counter/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `streakReduction` on the `Counter` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Counter" DROP COLUMN "streakReduction";
